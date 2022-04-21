cda7d72409976c9b86bcd9fb808a7e5031993af8
Leilani Quesada <leilanivquesada@gmail.com>
2022-04-21 12:08:38 -0700

    some of the exercises completed

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -1,7 +1,12 @@
 'use strict';
 
 function Homepage() {
-  return <div>This component needs to be implemented.</div>;
+  return <div>
+    <p>Welcome to my page! Are you sure you didn't come here on accident?</p>
+    <p>Hey, do you want to see my <a href="/cards">playing cards</a>? </p>
+    <img src="/static/img/balloonicorn.jpg" />
+    
+</div>;
 }
 
 ReactDOM.render(<Homepage />, document.querySelector('#app'));
--- static/js/tradingCard.jsx
+++ static/js/tradingCard.jsx
@@ -2,18 +2,73 @@
 
 const tradingCardData = [
   {
-    name: 'Balloonicorn',
-    skill: 'video games',
-    imgUrl: '/static/img/balloonicorn.jpg',
+    name: "Balloonicorn",
+    skill: "video games",
+    imgUrl: "/static/img/balloonicorn.jpg",
+    cardId: 1,
+  },
+  {
+    name: "Float",
+    skill: "baking pretzels",
+    imgUrl: "/static/img/float.jpg",
+    cardId: 2,
+  },
+  {
+    name: "Llambda",
+    skill: "knitting scarves",
+    imgUrl: "/static/img/llambda.jpg",
+    cardId: 3,
+  },
+  {
+    name: "Off-By-One",
+    skill: "climbing mountains",
+    imgUrl: "/static/img/off-by-one.jpeg",
+    cardId: 4,
+  },
+  {
+    name: "Seed.py",
+    skill: "making curry dishes",
+    imgUrl: "/static/img/seedpy.jpeg",
+    cardId: 5,
+  },
+  {
+    name: "Polymorphism",
+    skill: "costumes",
+    imgUrl: "/static/img/polymorphism.jpeg",
+    cardId: 6,
+  },
+  {
+    name: "Short Stack Overflow",
+    skill: "ocean animal trivia",
+    imgUrl: "/static/img/shortstack-overflow.jpeg",
+    cardId: 7,
+  },
+  {
+    name: "Merge",
+    skill: "bullet journaling",
+    imgUrl: "/static/img/merge.png",
+    cardId: 8,
   },
 ];
+
+
+console.log(tradingCardData);
+
+for (const card of tradingCardData){
+    console.log(tradingCardData.items()); 
+  }
+
+
+//   for (const card of tradingCardData); 
+//     console.log(tradingCardData['card']['name']); 
+// }
 
 function TradingCard(props) {
   return (
     <div className="card">
       <h2>Name: {props.name}</h2>
       <img src={props.imgUrl} alt="profile" />
-      <h2>Skill: </h2>
+      <h2>Skill: {props.skill} </h2>
     </div>
   );
 }
@@ -31,4 +86,14 @@ ReactDOM.render(
 ReactDOM.render(
   <TradingCard name="Llambda" skill="knitting scarves" imgUrl="/static/img/llambda.jpg" />,
   document.querySelector('#llambda'),
+);
+
+ReactDOM.render(
+  <TradingCard name="Merge" skill="making ice cubes" imgUrl="/static/img/merge.png" />,
+  document.querySelector('#merge'),
+);
+
+ReactDOM.render(
+  <TradingCard name="Seedpy" skill="smoke cigarettes" imgUrl="/static/img/seedpy.jpeg" />,
+  document.querySelector('#seedpy'),
 );
